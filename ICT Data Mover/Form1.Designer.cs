a7c9e8d5404a75c2c6b329feebf7ea34d1467a79
adbast <adbast@kitron.com>
2020-08-03 14:53:56 +0200

    final push

---
+++ ICT Data Mover/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace ICT_Data_Mover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.number = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.logName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.errorStatus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(284, 32);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(266, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sciezka do logow";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sciezka do serwera";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(12, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(266, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(284, 79);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.number,
+            this.logName,
+            this.errorStatus,
+            this.time});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(13, 122);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1164, 502);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // number
+            // 
+            this.number.Text = "Nr";
+            this.number.Width = 30;
+            // 
+            // logName
+            // 
+            this.logName.Text = "Nazwa pliku";
+            this.logName.Width = 600;
+            // 
+            // errorStatus
+            // 
+            this.errorStatus.Text = "Status";
+            this.errorStatus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.errorStatus.Width = 440;
+            // 
+            // time
+            // 
+            this.time.Text = "Godzina";
+            this.time.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.time.Width = 90;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Orange;
+            this.button1.Location = new System.Drawing.Point(332, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 64);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "OFF";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 636);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1188, 22);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 658);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "ICT Data Mover";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ColumnHeader number;
+        private System.Windows.Forms.ColumnHeader logName;
+        private System.Windows.Forms.ColumnHeader errorStatus;
+        private System.Windows.Forms.ColumnHeader time;
+    }
+}
+
---
+++ ICT Data Mover/Form1.cs
@@ -0,0 +1,445 @@
+﻿//VIRTUAL MODE - ABORTED
+//ERRORS FOLDER - DONE
+//CONFIG AS XML - CONFIG AS TXT DONE
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ICT_Data_Mover
+{
+    public partial class Form1 : Form
+    {
+        //INICJALIZACJA ZMIENNYCH      
+        public string logPath = "";
+        public string serverPath = "";
+        private string configFile = "config.txt";
+        public string errorFile = "errors.txt";
+        //PASS/FAIL COUNTERS
+        int logCounter = 0;
+        int passCounter = 0;
+        int failCounter = 0;
+        int allFailCounter = 0;
+        int allPassCounter = 0;
+        public string[] filenames;
+        public bool parsingOn = false;
+        public bool corruptedLog = false;
+        public string errorFolder = "errors\\";
+        //CONVERT MONTH NUMBER TO NAME
+        Dictionary<string, string> months = new Dictionary<string, string>()
+        {
+            {"JAN", "01"},
+            {"FEB", "02"},
+            {"MAR", "03"},
+            {"APR", "04"},
+            {"MAY", "05"},
+            {"JUN", "06"},
+            {"JUL", "07"},
+            {"AUG", "08"},
+            {"SEP", "09"},
+            {"OCT", "10"},
+            {"NOV", "11"},
+            {"DEC", "12"}
+        };
+        //CONFIG FILE
+        string appPath = AppDomain.CurrentDomain.BaseDirectory; //sciezka do folderu z plikiem exe
+        IniFile config = new IniFile("configuration.ini");
+
+        Timer timer;
+        bool timerEnabled = false;
+
+        //MAIN WINDOW CALL
+        public Form1()
+        {
+            InitializeComponent();
+            ReadConfigurationFile();
+            UpdateToolStrips(passCounter, allPassCounter, failCounter, allFailCounter);
+        }
+ 
+        //RETURN LIST OF FILENAMES IN FOLDER, CALLED AFTER EVERY TIMER TICK
+        private void LoadLogs(string dirInput, string dirOutput)
+        {
+            if (Directory.Exists(dirInput) && Directory.Exists(dirOutput))
+            {
+                filenames = Directory.GetFiles(dirInput);
+                string errorFolderCombined = appPath + errorFolder;
+                foreach (string filename in filenames)
+                {
+                    ProcessLog(filename, dirOutput);
+                    if (corruptedLog)
+                    {
+                        if (!Directory.Exists(errorFolderCombined))
+                        {
+                            Directory.CreateDirectory(errorFolderCombined);
+                        }
+                        File.Move(filename, errorFolderCombined+Path.GetFileName(filename));
+                    }
+                }
+            }
+            else
+            {
+                button1.PerformClick();
+                MessageBoxButtons buttons1 = MessageBoxButtons.YesNoCancel;
+                DialogResult result1;
+                result1 = MessageBox.Show("Foldery nie istnieja. Czy chcesz je utworzyc?", "Brak folderu o podanej nazwie", buttons1);
+                if (result1 == DialogResult.Yes)
+                {
+                    try
+                    {
+                        if (!Directory.Exists(dirInput))
+                        {
+                            Directory.CreateDirectory(dirInput);
+                        }
+                        if (!Directory.Exists(dirOutput))
+                        {
+                            Directory.CreateDirectory(dirOutput);
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Nie mozna utworzyc folderu");
+                    }
+                }
+            }
+        }
+
+        //PARSE 1 LOG AT A TIME
+        public void ProcessLog(string logName, string outputFolder)
+        {
+            //Husqv_BLDC_G2_SHO285257-20200604-133716-00016.LOG
+            //Husqv_BLDC_G2_SHO271541-20200310-200531-00001.LOG
+            string cutLogName = "";
+            string logYear = "";
+            string month = "";
+            string logMonth = "";
+            string logDay = "";
+            string logHour = "";
+            string logMin = "";
+            string logSeconds = "";
+            string lastLine = "";
+            string[] lines = File.ReadAllLines(logName);
+            string boardStatus = "";
+            string boardResult = "";
+            string sLogName = "";
+            string newLogName = "";
+            parsingOn = true;
+            try
+            {
+                lastLine = lines[lines.Length - 1];
+            }
+            //IF LOG IS INCOMPLETE, SKIP
+            catch(IndexOutOfRangeException oor)
+            {
+                SaveErrorToFile(oor.Message);
+                PopulateListView(logName, oor.Message, boardStatus);
+                if (!Directory.Exists("../error"))
+                {
+                    Directory.CreateDirectory("../error");
+                }
+                corruptedLog = true;
+                return;
+            }
+            boardResult = lastLine.Substring(0, 1);                         //PASS / FAIL
+            //Husqv_BLDC_G2_SHO285257.LOG_b2
+            //"04-JUN-20  19:22:33
+            sLogName = logName.Replace(textBox1.Text, "");                  //GET ONLY FILENAME FROM PATH
+            cutLogName = sLogName.Substring(1, sLogName.IndexOf(".")-1);    //REMOVE EXTENSION
+            //FORMATOWANIE DATY
+            var dateIndex = FindSep(lastLine, '-');  
+            //GET YEAR, MONTH AND DAY FROM FILE'S LAST LINE
+            logYear = lastLine.Substring(dateIndex[1]+1, 2);                
+            month = lastLine.Substring(dateIndex[0]+1, 3);
+            logMonth = months[month];
+            logDay = lastLine.Substring(1, 2);
+            //FORMAT TIME
+            var timeIndex = FindSep(lastLine, ':');
+            logHour = lastLine.Substring(timeIndex[0]-2, 2);
+            logMin = lastLine.Substring(timeIndex[0]+1, 2);
+            logSeconds = lastLine.Substring(timeIndex[1]+1, 2);
+            //CONCATENATE NEW FILENAME
+            newLogName = cutLogName + "-" + logYear + logMonth + logDay + "-" + logHour + logMin + logSeconds + ".LOG";
+            try
+            {
+                logCounter++;//do numerowania linii w listView
+                File.WriteAllLines(textBox2.Text + "\\" + newLogName, lines);       //WRITE FILE TO DESTINATION LOCATION
+                File.Delete(logName);                                               //REMOVE FILE IN SOURCE LOCATION
+                //iterowanie liczby pass i fail
+                if (boardResult == "\"")
+                {
+                    passCounter++;
+                    boardStatus = "Passed";
+                }
+                else
+                {
+                    failCounter++;
+                    boardStatus = "Failed";
+                }
+                PopulateListView(newLogName, "  OK!", boardStatus);
+                UpdateToolStrips(passCounter, allPassCounter, failCounter, allFailCounter); //update po kazdym logu
+            }
+            catch(Exception e)
+            {
+                PopulateListView(newLogName, e.Message);
+                SaveErrorToFile(e.Message);
+            }
+            parsingOn = false;
+        }
+ 
+        //**********OKNO PRZY ZAMYKANIU APLIKACJI*************
+        public void ReadConfigurationFile()
+        {
+            try
+            {
+                //CHECK IF CONFIG CONTAIN VARIABLES
+                if (!config.KeyExists("Local folder", "Paths"))
+                {
+                    config.Write("Local folder", "C:\\", "Paths");
+                }
+                if (!config.KeyExists("Server folder", "Paths"))
+                {
+                    config.Write("Server folder", "C:\\", "Paths");
+                }
+                if (!config.KeyExists("Passed boards", "Counters"))
+                {
+                    config.Write("Passed boards", "0", "Counters");
+                }
+                if (!config.KeyExists("Failed boards", "Counters"))
+                {
+                    config.Write("Failed boards", "0", "Counters");
+                }
+
+                logPath = config.Read("Local folder", "Paths");
+                serverPath = config.Read("Server folder", "Paths");
+                allPassCounter = int.Parse(config.Read("Passed boards", "Counters"));
+                allFailCounter = int.Parse(config.Read("Failed boards", "Counters"));
+                textBox1.Text = logPath;
+                textBox2.Text = serverPath;
+            }
+            catch(Exception  e)
+            {
+                string msg = "Brak uprawnien do utworzenia lub nadpisania pliku configuration.ini. " +
+                    "Sprawdz czy aplikacja nie znajduje sie w folderze systemowym( C:\\, C:\\Program Files itp..)";
+                SaveErrorToFile(e.Message);
+                MessageBox.Show(msg);
+            }
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            //NOT IMPLEMENTED
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBox1.Enabled = false;
+            }
+            else
+            {
+                textBox1.Enabled = true;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                textBox2.Enabled = false;
+            }
+            else
+            {
+                textBox2.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "ON")
+            {
+                button1.Text = "OFF";
+                button1.BackColor = Color.Orange;
+                StopTimer();
+            }
+            else if (button1.Text == "OFF")
+            {
+                button1.Text = "ON";
+                button1.BackColor = Color.LimeGreen;
+                StartTimer();
+            }
+        }
+
+        public void PopulateListView(string parsedLog, string errCode, string bstat = "")
+        {
+            ListViewItem item1 = new ListViewItem(logCounter.ToString());
+            item1.SubItems.Add(parsedLog);
+            item1.SubItems.Add(errCode);
+            item1.SubItems.Add(DateTime.Now.ToLongTimeString());
+            listView1.Items.Insert(0, item1);
+            if (bstat == "Failed")
+            {
+                listView1.Items[0].BackColor = Color.OrangeRed;
+            }
+            else if (bstat == "Passed")
+            {
+                listView1.Items[0].BackColor = Color.Lime;
+            }
+
+        }
+        public void UpdateToolStrips(int pCounter, int apCounter, int fCounter, int afCounter)
+        {
+            toolStripStatusLabel1.Text = "Od uruchomienia: " + pCounter + " pass | " + fCounter + " fail";
+            toolStripStatusLabel2.Text = "Wszystkie: " + apCounter + " pass | " + afCounter + " fail";
+        }
+        //CHECKS IF CURRENT PATHS ARE DIFFERENT THAN ONES STORED IN CONFIG FILE
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+            string message = "";
+            string caption = "";
+            if (textBox1.Text != logPath || textBox2.Text != serverPath)
+            {
+                message = "Plik config rozni sie od sciezek podanych w programie. Czy chcesz zapisac zmiany w pliku config?";
+                caption = "Zmieniono sciezke do logow lub serwera";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNoCancel;
+                DialogResult result;
+                result = MessageBox.Show(message, caption, buttons);
+                if (result == DialogResult.Yes)
+                {
+                    config.Write("Local folder", textBox1.Text, "Paths");
+                    config.Write("Server folder", textBox2.Text, "Paths");
+                }
+                if (result == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+            }
+            allFailCounter += failCounter;
+            allPassCounter += passCounter;
+            try
+            {
+                config.Write("Passed boards", allPassCounter.ToString(), "Counters");
+                config.Write("Failed boards", allFailCounter.ToString(), "Counters");
+            }
+            catch
+            {
+                message = "Brak uprawnien do nadpisania pliku config. Sprawdz czy aplikacja nie znajduje sie w folderze systemowym.";
+                SaveErrorToFile(message);
+                MessageBox.Show(message);
+            }
+        }
+
+        //******LOG PARSE CALLER**********
+        void Timer_Tick(object sender, EventArgs e)
+        {
+            LoadLogs(textBox1.Text, textBox2.Text);
+        }
+        //TIMER FUNCTIONS USED TO CALCULATE TIME BETWEEN PARSING CALLS
+        public void StartTimer()
+        {
+            if (!timerEnabled && !parsingOn)
+            {
+                timer = new Timer();
+                timer.Interval = 10000;
+                timer.Tick += Timer_Tick;
+                timer.Start();
+                timerEnabled = true;
+            }
+        }
+
+        public void StopTimer()
+        {
+            timer.Stop();
+            timerEnabled = false;
+        }
+
+        //FIND SEPARATOR USED IN TEXT
+        public List<int> FindSep(string line, char sep)
+        {
+            var separator = new List<int>();
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == sep)
+                {
+                    separator.Add(i);
+                }
+            }
+            return separator;
+        }
+        //STORE ERRORS FOR DEBUG
+        public void SaveErrorToFile(string errorMessage)
+        {
+            if (!File.Exists(errorFile))
+            {
+                using (StreamWriter sw = File.CreateText(errorFile)) ;
+            }
+            using (StreamWriter sw = File.AppendText(errorFile))
+            {
+                sw.WriteLine(DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + " " + errorMessage);
+            }
+        }
+
+        //******RESIDUAL CODE********************
+        //******CONFIG WCZYTYWANY PRZEZ READCONFIGURATIONFILE***********
+        private void LoadConfigFile()
+        {
+            try
+            {
+                string currentLine = "";
+                FileStream FS = new FileStream(configFile, FileMode.Open);
+                using (StreamReader SR = new StreamReader(FS))
+                {
+                    while (SR.Peek() > 0)
+                    {
+                        currentLine = SR.ReadLine();
+                        if (currentLine.StartsWith("log_path"))
+                        {
+                            logPath = currentLine.Substring(currentLine.IndexOf("=") + 2);
+                        }
+                        if (currentLine.StartsWith("server_path"))
+                        {
+                            serverPath = currentLine.Substring(currentLine.IndexOf("=") + 2);
+                        }
+                        if (currentLine.StartsWith("All Pass"))
+                        {
+                            allPassCounter = int.Parse(currentLine.Substring(currentLine.IndexOf(":") + 1));
+                        }
+                        if (currentLine.StartsWith("All Fail"))
+                        {
+                            allFailCounter = int.Parse(currentLine.Substring(currentLine.IndexOf(":") + 1));
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                string message = e.Message;
+                SaveErrorToFile(message);
+                string caption = "Blad podczas wczytywania";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+                result = MessageBox.Show(message, caption, buttons);
+                if (result == DialogResult.OK)
+                {
+                    try
+                    {
+                        FileStream FS = new FileStream(configFile, FileMode.Create);
+                        FS.Close();
+                    }
+                    catch (Exception ee)
+                    {
+                        message = "Brak uprawnien do utworzenia pliku config. Sprawdz czy aplikacja nie znajduje sie w folderze systemowym.   ";
+                        SaveErrorToFile(message + ee.Message);
+                        MessageBox.Show(message);
+                    }
+                }
+            }
+            textBox1.Text = logPath;  //wpisanie sciezek do okien textbox - mozna pozniej zmienic przed wlaczeniem parsowania
+            textBox2.Text = serverPath;
+        }
+    }
+}
